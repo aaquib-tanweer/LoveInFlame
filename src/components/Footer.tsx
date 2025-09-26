26dbc5b5635b9dbea51b91521b0d2a339905223b
Aaquib Tanweer <aaquib.tanweer.111@gmail.com>
2025-09-27 00:46:02 +0530

    Update Footer.tsx
    footer typo fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return <footer className="relative z-10 px-6 py-8 text-center text-white/80">
       <div className="max-w-6xl mx-auto">
         <div className="flex items-center justify-center text-sm">
-          <span>© 2024 Love In Flame. All rights reserved.</span>
+          <span>© 2025 Love In Flame. All rights reserved.</span>
           <HeartIcon className="w-4 h-4 mx-2 text-pink-400" />
           <span>Made with love</span>
         </div>
